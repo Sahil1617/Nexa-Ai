cc78b0fd857d9296d602313a323ee0e8ecfbd8df
Sahil Jadhav <sahiljadhav1617@gmail.com>
2025-08-17 13:59:23 +0530

    Chat Functionality Integrated

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import "./ChatWindow.css"
+import Chat from "./Chat.jsx"
+import { MyContext } from "./MyContext.jsx"
+import { useContext, useState } from "react"
+import {RingLoader} from "react-spinners"
 
 const ChatWindow = () => {
+  const {prompt, setPrompt, reply, setReply, currThreadId} = useContext(MyContext);
+  const [loding, setLoding] = useState(false);
+  const getReply = async () => {
+    setLoding(true);
+    const options = {
+      method : "POST",
+      headers : {
+        "Content-Type" : "application/json",
+      },
+      body: JSON.stringify({
+        message: prompt,
+        threadId: currThreadId,
+      })
+    };
+    try {
+      const response = await fetch("http://localhost:8000/api/chat", options);
+      const res = await response.json();
+      console.log(res);
+      setReply(res.reply);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoding(false);
+  }
   return (
-    <div>ChatWindow</div>
+    <div className='chatWindow'>
+      <div className='navbar'>
+        <span>Nexa AI<i className="fa-solid fa-chevron-down"></i></span>
+        <div className='userIconDiv'>
+          <span className='userIcon'><i className="fa-solid fa-user"></i></span>
+        </div>
+      </div>
+    
+      <Chat/>
+
+      <RingLoader color='#84c2fcff' loading={loding}></RingLoader>
+
+      <div className='chatInput'>
+        <div className='inputBox'>
+          <input type="text" placeholder='Ask Anything'  value={prompt} onChange={(e) => setPrompt(e.target.value)} onKeyDown={(e) => e.key === 'Enter' ? getReply(): ''}/>
+          <div id='submit' onClick={getReply}><i className="fa-solid fa-paper-plane"></i></div>
+        </div>
+        <p className='info'>Nexa AI can help you with anything. You can ask any question.</p>
+      </div>
+    </div>
   )
 }
 
